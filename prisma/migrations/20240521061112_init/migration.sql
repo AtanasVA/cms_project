52b79c0ed096cb87053a2b7756c2862b9b763521
Atanas Vasilev <my.atanas.vasilev@gmail.com>
2024-05-21 09:13:37 +0300

    run migrations for relation with posts table

---
+++ prisma/migrations/20240521061112_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE `pages` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `slug` VARCHAR(191) NOT NULL,
+    `meta_title` VARCHAR(191) NOT NULL,
+    `meta_description` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `pages_slug_key`(`slug`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `posts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `post_content` VARCHAR(191) NULL,
+    `parentSlug` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `posts` ADD CONSTRAINT `posts_parentSlug_fkey` FOREIGN KEY (`parentSlug`) REFERENCES `pages`(`slug`) ON DELETE RESTRICT ON UPDATE CASCADE;
