866060eec083370c9ea826a43b682b95de984afa
Atanas Vasilev <my.atanas.vasilev@gmail.com>
2024-06-02 10:40:07 +0300

    Temp remove pre-rendered query for deployment to complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import Landing from "./Landing/page";
 import { getPages } from "utils/queries";
 
 const Home = async () => {
-  const data = await getPages();
+  // const data = await getPages();
+  const data = [] as any;
   return <Landing pagesData={data} />;
 };
 
